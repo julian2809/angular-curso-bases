cd0877b769431ad81b5f4a6d3c836bb1cbbb91df
julian2809 <julian2809@gmail.com>
2022-12-01 21:54:34 -0600

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { contadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+    contadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
